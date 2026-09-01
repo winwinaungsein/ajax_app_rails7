52491c6022bf01b55710cba9c03dd30ec10506d6
Win Win Aung sein <winwinaungsein@gmail.com>
2026-09-01 18:22:16 +0900

    Add render-build.sh

--- Gemfile
+++ Gemfile
@@ -62,6 +62,9 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+  group :production do
+  gem 'pg'
+end
 end
 
 group :test do
---
+++ app/javascript/count.js
@@ -0,0 +1,8 @@
+unction count (){
+  const articleText  = document.getElementById("article_text");
+  articleText.addEventListener("keyup", () => {
+    console.log(articleText.value.length);
+  });
+};
+
+window.addEventListener('turbo:load', count);
